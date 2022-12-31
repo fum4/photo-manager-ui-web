4efab6c0d870a3e2133d10c7e5bb1d5a44490e8c
Isis☕Caffe <52106389+fum4@users.noreply.github.com>
2022-12-31 10:20:22 +0200

    UX / Style improvements - Expand drop zone to cover the whole document

--- src/app/directives/drag-and-drop.directive.ts
+++ src/app/directives/drag-and-drop.directive.ts
@@ -7,21 +7,24 @@ export class DragAndDropDirective {
   @HostBinding('class.dragging-over') isDraggingOver = false;
   @Output() fileDropped = new EventEmitter<File[]>();
 
-  @HostListener('dragover', ['$event']) onDragOver(event: DragEvent) {
+  @HostListener('dragover', [ '$event' ])
+  onDragOver = (event: DragEvent) => {
     event.preventDefault();
     event.stopPropagation();
 
     this.isDraggingOver = true;
   }
 
-  @HostListener('dragleave', ['$event']) public onDragLeave(event: DragEvent) {
+  @HostListener('dragleave', [ '$event' ])
+  onDragLeave = (event: DragEvent) => {
     event.preventDefault();
     event.stopPropagation();
 
     this.isDraggingOver = false;
   }
 
-  @HostListener('drop', ['$event']) public onDrop(event: DragEvent) {
+  @HostListener('drop', [ '$event' ])
+  onDrop = (event: DragEvent) => {
     event.preventDefault();
     event.stopPropagation();
 
